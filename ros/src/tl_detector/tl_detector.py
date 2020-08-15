1deda30547944a4482be070ac21a2970abd72eff
yuki678 <uk.12juni@gmail.com>
2020-08-15 10:36:09 +0100

    fix: Test traffic light detector

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -13,6 +13,7 @@ import yaml
 from scipy.spatial import KDTree
 
 STATE_COUNT_THRESHOLD = 3
+CLF_ENABLED = False
 
 class TLDetector(object):
     def __init__(self):
@@ -124,16 +125,19 @@ class TLDetector(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        if(not self.has_image):
-            self.prev_light_loc = None
-            return False
+        if CLF_ENABLED:
 
-        cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
+            if(not self.has_image):
+                self.prev_light_loc = None
+                return False
 
-        #Get classification
-        return self.light_classifier.get_classification(cv_image)
+            cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
 
-        # return light.state
+            #Get classification
+            return self.light_classifier.get_classification(cv_image)
+
+        else:
+            return light.state
 
     def process_traffic_lights(self):
         """Finds closest visible traffic light, if one exists, and determines its
