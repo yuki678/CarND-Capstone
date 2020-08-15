039f4f11663933eb883da0035e625f918b4b069b
yuki678 <uk.12juni@gmail.com>
2020-08-15 07:33:01 +0100

    fix: use filt() method of LowPassFilter

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -40,7 +40,7 @@ class Controller(object):
             self.throttle_controller.reset()
             return 0., 0., 0.
 
-        current_vel = self.vel_lpf(current_vel)
+        current_vel = self.vel_lpf.filt(current_vel)
 
         steering = self.yaw_controller.get_steering(linear_vel, angular_vel, current_vel)
 
