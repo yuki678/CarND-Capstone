8822fa8d2a3da3576ae356c5a5976245fb88dcd9
yuki678 <uk.12juni@gmail.com>
2020-08-29 00:09:51 +0100

    Reduced the frequency to 10

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -53,7 +53,7 @@ class WaypointUpdater(object):
         self.loop()
 
     def loop(self):
-        rate = rospy.Rate(30)
+        rate = rospy.Rate(10)
         while not rospy.is_shutdown():
             if self.pose and self.base_lane:
                 # closest_waypoint_idx = self.get_closest_waypoint_idx()
