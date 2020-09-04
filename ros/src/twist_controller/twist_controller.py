10e1df50fcaf38ff76b42714458fdce9e2fa62c7
yuki678 <uk.12juni@gmail.com>
2020-09-04 11:44:57 +0100

    Add ONE_MPH into the calculation

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -45,7 +45,7 @@ class Controller(object):
 
         steering = self.yaw_controller.get_steering(linear_vel, angular_vel, current_vel)
 
-        vel_error = linear_vel - current_vel
+        vel_error = linear_vel / ONE_MPH - current_vel
         self.last_vel = current_vel
 
         current_time = rospy.get_time()
