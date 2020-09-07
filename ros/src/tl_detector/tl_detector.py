4b3876f3fe902d65259b44f0aa972da6406c0ef3
yuki678 <uk.12juni@gmail.com>
2020-09-07 17:52:56 +0100

    update the trained model

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -17,7 +17,7 @@ CLF_ENABLED = True
 # CLF_NAME = 'faster_rcnn_resnet101_coco_inference_graph_sim.pb'
 # CLF_NAME = 'faster_rcnn_resnet101_coco_inference_graph_site.pb'
 # CLF_NAME = 'tf1_ssd_mobilenet_v2_sim.pb'
-CLF_NAME = 'optimzed_inference_graph.pb'
+CLF_NAME = 'tf1_faster_rcnn_resnet50_coco_sim.pb'
 
 class TLDetector(object):
     def __init__(self):
