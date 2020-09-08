9a2ca5f454503ab078e006cbb9622325f552a3c6
yuki678 <uk.12juni@gmail.com>
2020-09-08 22:42:21 +0100

    A simple script to repeatedly update the same name image

---
+++ ros/src/tl_detector/show_image.py
@@ -0,0 +1,10 @@
+import cv2
+#import matplotlib.pyplot as plt
+#im_resized = cv2.resize(im, (224, 224), interpolation=cv2.INTER_LINEAR)
+#plt.imshow(cv2.cvtColor(im_resized, cv2.COLOR_BGR2RGB))
+#plt.show()
+
+while True:
+    img = cv2.imread('images/image.png')
+    cv2.imshow('image', img)
+    cv2.waitKey(1)
