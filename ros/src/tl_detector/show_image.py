6ba98b12c961c43c2c6e1e0088cc1ba0ab5362e0
yuki678 <uk.12juni@gmail.com>
2020-09-08 22:59:15 +0100

    Rename the image file

--- ros/src/tl_detector/show_image.py
+++ ros/src/tl_detector/show_image.py
@@ -4,7 +4,14 @@ import cv2
 #plt.imshow(cv2.cvtColor(im_resized, cv2.COLOR_BGR2RGB))
 #plt.show()
 
-while True:
-    img = cv2.imread('images/image.jpg')
-    cv2.imshow('image', img)
-    cv2.waitKey(1)
+# while True:
+#     img = cv2.imread('images/image.jpg')
+#     cv2.imshow('image', img)
+#     cv2.waitKey(1)
+
+
+cv2.namedWindow("output", cv2.WINDOW_NORMAL)         # Create window with freedom of dimensions
+im = cv2.imread('images/image.jpg')                  # Read image
+imS = cv2.resize(im, (400, 300))                     # Resize image
+cv2.imshow("output", imS)                            # Show image
+cv2.waitKey(0)                                       # Display the image infinitely until any keypress
